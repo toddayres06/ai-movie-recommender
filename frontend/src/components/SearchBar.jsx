9b6fe0b0a5b5629ba887634e7fcb39f4b536ce7c
Your Name <you@example.com>
2026-03-28 08:05:56 -0500

    started Searchbar component

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+import { useState} from "react"
+
+function SearchBar({onSearch}) {
+    const [query, setQuery] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onSearch(query)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="flex gap-2">
+            <input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search a movie..."
+                className="px-4 py-2 rounded text-black"
+            />
+
+            <button className="bg-blue-600 px-4 py-2 rounded">
+                Search
+            </button>
+        </form>
+    )
+}
+
+export default SearchBar
