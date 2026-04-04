384999634671ea37434f82f0079e4c03d7411143
Your Name <you@example.com>
2026-04-04 08:12:10 -0500

    prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ app.post("/recommend", async (req, res) => {
 
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
